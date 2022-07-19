cbeaf897cf550e34a4908bddef4b20a7f354c749
Amanda <88062177+amandahn@users.noreply.github.com>
2022-07-18 20:58:49 -0700

    Update BinaryImage.h

--- ImageProcessor/BinaryImage.h
+++ ImageProcessor/BinaryImage.h
@@ -18,25 +18,7 @@ typedef struct {
     unsigned int cols;
 } img_t;
 
-
-/*
-  Save the entire 2D array in im into a file called 'filename' in a binary
-  file format that can be loaded by img_load_binary(). Returns
-  zero on success, or a non-zero error code on failure. Arrays of
-  length 0 should produce an output file containing an empty array.
-
-  Make sure you validate the parameters before you use them.
-*/
-
 int img_save_binary(img_t* im, const char* filename);
-
-/*
-  Load a new 2D array from the file called 'filename', that was
-  previously saved using img_save_binary(). Returns a pointer to a
-  newly-allocated img_t on success, or NULL on failure.
-
-  Make sure you validate the parameter before you use it.
-*/
 
 img_t* img_load_binary(const char* filename);
 
